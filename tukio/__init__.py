9ea1b360dadf1bb3e8c002b7272cc9d5a4b853de
Ludovic Havel <ludovic.havel@surycat.com>
2016-05-18 14:57:51 +0200

    wrapped long running operations of the engine in tasks and added a 'cancel()' method

---
+++ tukio/__init__.py
@@ -0,0 +1,2 @@
+from .engine import Engine
+from .workflow import WorkflowTemplate, Workflow
--- tukio/engine.py
+++ tukio/engine.py
@@ -2,6 +2,7 @@
 Tukio Workflow Engine
 """
 import asyncio
+import weakref
 
 from tukio.workflow import WorkflowTemplate, OverrunPolicy, new_workflow
 from tukio.broker import get_broker
@@ -18,34 +19,62 @@ class Engine:
         self._running = dict()
         self._broker = get_broker(self._loop)
         self._lock = asyncio.Lock()
-        self._lock = asyncio.Lock()
+        self._running_by_id = weakref.WeakValueDictionary()
+
+    def _add_wflow(self, wflow):
+        """
+        Adds a new entry into the dict of running workflows and updates the
+        weak value dict to index it by its execution ID.
+        """
+        try:
+            self._running[wflow.template_id].append(wflow)
+        except KeyError:
+            self._running[wflow.template_id] = [wflow]
+        self._running_by_id[wflow.uid] = wflow
+
+    def _remove_wflow(self, wflow):
+        """
+        Removes a worflow instance from the dict of running workflows.
+        """
+        self._running[wflow.template_id].remove(wflow)
+        # Cleanup the running dict if no more running instance of that template
+        if len(self._running[wflow.template_id]) == 0:
+            del self._running[wflow.template_id]
+        del self._running_by_id[wflow.uid]
 
     def stop(self):
         pass
 
-    def _load(self, templates):
+    def _run_in_task(self, callback, *args, **kwargs):
         """
-        Loads a list of workflow templates into the engine. Each workflow may
-        be triggered as soon as it is loaded.
-        Each item of the `templates` list must be a dictionary that represents
-        a workflow template.
+        Wrap a regular function into a coroutine and run it in a task.
+        This is intended to wrap time consuming functions into a task so as to
+        prevent slow operations from blocking the whole loop.
+        """
+        async def coro():
+            return callback(*args, **kwargs)
+        return asyncio.ensure_future(coro(), loop=self._loop)
+
+    def _load(self, tmpl_dict):
+        """
+        Loads a workflow template into the engine. Each workflow may be
+        triggered as soon as it is loaded.
         Duplicates or invalid descriptions raise an exception.
         This operation does not affect workflow executions in progress.
         """
-        for tmpl_dict in templates:
-            wf_tmpl = WorkflowTemplate.from_dict(tmpl_dict)
-            # A workflow template is uniquely defined by the tuple
-            # (template ID, version)
-            duplicate = None
-            try:
-                duplicate = self._templates[wf_tmpl.uid]
-            except KeyError:
-                self._templates[wf_tmpl.uid] = wf_tmpl
+        wf_tmpl = WorkflowTemplate.from_dict(tmpl_dict)
+        # A workflow template is uniquely defined by the tuple
+        # (template ID, version)
+        duplicate = None
+        try:
+            duplicate = self._templates[wf_tmpl.uid]
+        except KeyError:
+            self._templates[wf_tmpl.uid] = wf_tmpl
+        else:
+            if duplicate.version == wf_tmpl.version:
+                raise DuplicateWorkflowError
             else:
-                if duplicate.version == wf_tmpl.version:
-                    raise DuplicateWorkflowError
-                else:
-                    self._templates[wf_tmpl.uid] = wf_tmpl
+                self._templates[wf_tmpl.uid] = wf_tmpl
 
     async def load(self, templates):
         """
@@ -54,7 +83,8 @@ class Engine:
         time.
         """
         with await self._lock:
-            self._load(templates)
+            for tmpl_dict in templates:
+                await self._run_in_task(self._load, tmpl_dict)
 
     async def reload(self, templates):
         """
@@ -63,7 +93,7 @@ class Engine:
         """
         with await self._lock:
             self._templates = dict()
-            self.load(templates)
+        await self.load(templates)
 
     async def data_received(self, data):
         """
@@ -73,21 +103,10 @@ class Engine:
         """
         self._broker.dispatch(data)
         with await self._lock:
-            # Try to run new workflow instances at all times
-            for tmpl_id in self._running:
-                self._try_run(tmpl_id, data)
-            # Trigger workflows from templates that has no running instance
-            idle_tmpl_ids = self._get_idle_tmpl_ids()
-            for tmpl_id in idle_tmpl_ids:
-                self._try_run(tmpl_id, data)
-
-    def _get_idle_tmpl_ids(self):
-        """
-        Returns the set of templates that has no running instance.
-        """
-        template_ids = set(self._templates.keys())
-        running_tmpl_ids = set(self._running.keys())
-        return template_ids - running_tmpl_ids
+            # Try to trigger new workflows from the current dict of workflow
+            # templates at all times!
+            for tmpl_id in self._templates:
+                await self._run_in_task(self._try_run, tmpl_id, data)
 
     def _try_run(self, tmpl_id, data):
         """
@@ -105,13 +124,14 @@ class Engine:
         # instances may run with an old version of the template)
         wflow = new_workflow(wf_tmpl, running=running, loop=self._loop)
         if wflow:
+            wflow.add_done_callback(self._clean_done)
             if wf_tmpl.policy == OverrunPolicy.abort_running and running:
                 def cb():
                     wflow.run(data)
                 asyncio.ensure_future(self._wait_abort(running, cb))
             else:
                 wflow.run(data)
-            self._running[tmpl_id].append(wflow)
+            self._add_wflow(wflow)
 
     async def _wait_abort(self, running, callback):
         """
@@ -121,7 +141,22 @@ class Engine:
         await asyncio.wait(running)
         callback()
 
+    def _clean_done(self, future):
+        """
+        Cleanup actions when a workflow is done.
+        """
+        async def with_lock():
+            with await self._lock:
+                self._remove_wflow(future)
+        asyncio.ensure_future(with_lock())
+
     def cancel(self, exec_id):
         """
         Cancels an execution of workflow identified by its execution ID.
+        The cancelled workflow instance (a future object) is returned.
+        If the workflow could not be found, returns None.
         """
+        wflow = self._running_by_id.get(exec_id)
+        if wflow and not wflow.done():
+            wflow.cancel()
+        return wflow
