fb4af5f7e25230e0a846bc42e899e022c494b588
Laurent Erignoux <lerignoux@gmail.com>
2016-05-19 16:15:20 +0200

    Adding pypy build files

---
+++ deploy_to_pypi.sh
@@ -0,0 +1,11 @@
+echo -e "[distutils]
+index-servers=pypi
+
+[pypi]
+repository = https://pypi.python.org/pypi
+username = $PYPI_USER
+password = $PYPI_PASSWORD
+" > ~/.pypirc
+
+echo $(git describe --abbrev=0 --tags) > VERSION.txt
+python setup.py sdist upload
