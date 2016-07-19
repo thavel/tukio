74175e4808da1d174d24bdbca5b88e89de9f16aa
Ludovic Havel <ludovic.havel@surycat.com>
2016-07-19 18:41:39 +0200

    updated __init__ modules to ease imports

--- tukio/__init__.py
+++ tukio/__init__.py
@@ -1,3 +1,4 @@
 from .engine import Engine
-from .task import TaskRegistry, TaskHolder, JoinTask, UnknownTaskName
+from .task import TaskRegistry, TaskHolder, JoinTask, UnknownTaskName, dispatch_progress
 from .workflow import WorkflowTemplate, Workflow, WorkflowInterface
+from .event import Event, EventSource
--- tukio/task/__init__.py
+++ tukio/task/__init__.py
@@ -1,5 +1,5 @@
 from .join import JoinTask
-from .task import TaskRegistry, UnknownTaskName, register, new_task, TaskExecState
+from .task import TaskRegistry, UnknownTaskName, register, new_task, dispatch_progress
 from .factory import TukioTask, tukio_factory
 from .holder import TaskHolder
 from .template import TaskTemplate
