4af595d39c382c8d6b88d68e250fce8ad45862e0
Ludovic Havel <ludovic.havel@surycat.com>
2016-07-20 18:41:31 +0200

    added tukio.broker imports to __init__.py

--- tukio/__init__.py
+++ tukio/__init__.py
@@ -2,3 +2,4 @@ from .engine import Engine
 from .task import TaskRegistry, TaskHolder, JoinTask, UnknownTaskName, dispatch_progress
 from .workflow import WorkflowTemplate, Workflow
 from .event import Event, EventSource
+from .broker import get_broker, EXEC_TOPIC
